05534087e1ea6722dc8561a290d765bae11e980f
PixelCircuits <circuitpixels@gmail.com>
2023-06-20 10:40:04 -0400

    Added basic contract layout

---
+++ test/EntryPoint.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/EntryPoint.sol";
+
+contract EntryPointTest is Test {
+    EntryPoint public entrypoint;
+
+    function setUp() public {
+        entrypoint = new EntryPoint();
+        entrypoint.setNumber(0);
+    }
+
+    function testIncrement() public {
+        entrypoint.increment();
+        assertEq(entrypoint.number(), 1);
+    }
+
+    function testSetNumber(uint256 x) public {
+        entrypoint.setNumber(x);
+        assertEq(entrypoint.number(), x);
+    }
+}
