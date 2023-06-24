b1f5786220157b93015b6018c71a495fdf31b79e
PixelCircuits <circuitpixels@gmail.com>
2023-06-24 16:25:32 -0400

    fixed compile errors

--- test/EntryPoint.t.sol
+++ test/EntryPoint.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/EntryPoint.sol";
+import "../src/core/EntryPoint.sol";
 
 contract EntryPointTest is Test {
     EntryPoint public entrypoint;
 
     function setUp() public {
         entrypoint = new EntryPoint();
-        entrypoint.setNumber(0);
+        //entrypoint.setNumber(0);
     }
 
     function testIncrement() public {
-        entrypoint.increment();
-        assertEq(entrypoint.number(), 1);
+        //entrypoint.increment();
+        //assertEq(entrypoint.number(), 1);
     }
 
     function testSetNumber(uint256 x) public {
-        entrypoint.setNumber(x);
-        assertEq(entrypoint.number(), x);
+        //entrypoint.setNumber(x);
+        //assertEq(entrypoint.number(), x);
     }
 }
