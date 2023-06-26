afe15fe26a340aa95bf8fb9f1b4b8d977a148000
Bikem <bengisuozaydin@gmail.com>
2023-06-26 23:28:23 +0100

    wallet skeleton

---
+++ src/wallet/SimpleAccount.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+import "../core/BaseAccount.sol";
+import "../interfaces/IAssetRelease.sol";
+import "./TokenCallbackHandler.sol";
+
+/**
+ * minimal account.
+ *  this is sample minimal account.
+ *  has execute, eth handling methods
+ *  has a single signer that can send requests through the entryPoint.
+ */
+contract SimpleAccount is BaseAccount, TokenCallbackHandler, IAssetRelease {
+    using ECDSA for bytes32;
+
+    address public owner;
+
+    IEntryPoint private immutable _entryPoint;
+
+    // TODO: here temporarily
+    bytes4 private constant ERC20_TRANSFER = bytes4(keccak256(bytes("transfer(address,uint256)")));
+    bytes4 private constant ERC721_SAFE_TRANSFER_FROM =
+        bytes4(keccak256(bytes("safeTransferFrom(address,address,uint256)")));
+
+    event SimpleAccountInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+
+    modifier onlyOwner() {
+        _onlyOwner();
+        _;
+    }
+
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+    }
+
+    function _onlyOwner() internal view {
+        //directly from EOA owner, or through the account itself (which gets redirected through execute())
+        require(msg.sender == owner || msg.sender == address(this), "only owner");
+    }
+
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     */
+    function execute(address dest, uint256 value, bytes calldata func) external {
+        _requireFromEntryPointOrOwner();
+        _call(dest, value, func);
+    }
+
+    /**
+     * execute a sequence of transactions
+     */
+    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
+        _requireFromEntryPointOrOwner();
+        require(dest.length == func.length, "wrong array lengths");
+        for (uint256 i = 0; i < dest.length; i++) {
+            _call(dest[i], 0, func[i]);
+        }
+    }
+
+    function releaseAsset(uint256 assetType, uint256 assetId, uint256 amount) external {
+        _requireFromEntryPointOrOwner();
+        address payable receiver = payable(msg.sender);
+
+        address token = address(uint160(uint256(assetId)));
+
+        // TODO: map assetTypes to actual types
+        if (assetType == 0) {
+            _call(receiver, amount, "");
+        } else if (assetType == 1) {
+            _call(token, 0, abi.encodeWithSelector(ERC20_TRANSFER, receiver, amount));
+        } else if (assetType == 2) {
+            _call(token, 0, abi.encodeWithSelector(ERC721_SAFE_TRANSFER_FROM, owner, receiver, amount));
+        } else {
+            revert("Unsupported asset type");
+        }
+    }
+
+    // Require the function call went through EntryPoint or owner
+    function _requireFromEntryPointOrOwner() internal view {
+        require(msg.sender == address(entryPoint()) || msg.sender == owner, "account: not Owner or EntryPoint");
+    }
+
+    /// implement template method of BaseAccount
+    function _validateSignature(UserIntent calldata userInt, bytes32 userIntHash)
+        internal
+        virtual
+        override
+        returns (uint256 validationData)
+    {
+        bytes32 hash = userIntHash.toEthSignedMessageHash();
+        if (owner != hash.recover(userInt.signature)) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return 0;
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+}
