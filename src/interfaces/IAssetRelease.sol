104ba3daffcf18a43e463074aa2a90b33b8f9e97
PixelCircuits <circuitpixels@gmail.com>
2023-06-27 17:56:30 -0400

    added support for more token types

--- src/interfaces/IAssetRelease.sol
+++ src/interfaces/IAssetRelease.sol
@@ -5,6 +5,7 @@ enum AssetType {
     ETH,
     ERC20,
     ERC721,
+    ERC721_ID,
     ERC777,
     ERC1155,
     COUNT
@@ -17,7 +18,7 @@ interface IAssetRelease {
      * @dev Must validate caller is the entryPoint.
      * @param assetType the type of asset (ETH, ERC-20, ERC721, etc).
      * @param assetContract the contract that controls the asset.
-     * @param assetId the identifier for the specific asset.
+     * @param assetId the identifier for a specific asset.
      * @param amount the amount to release.
      */
     function releaseAsset(AssetType assetType, address assetContract, uint256 assetId, uint256 amount) external;
