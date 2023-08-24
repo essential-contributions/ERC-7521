caaa8aa3578017efd8f514c0627bbafd9db5ced8
Bikem <bengisuozaydin@gmail.com>
2023-08-24 20:08:27 +0100

    solhint nits

--- src/wallet/AbstractAccount.sol
+++ src/wallet/AbstractAccount.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/* solhint-disable private-vars-leading-underscore */
+
 import {BaseAccount} from "../core/BaseAccount.sol";
 import {IEntryPoint} from "../interfaces/IEntryPoint.sol";
 import {IIntentDelegate} from "../interfaces/IIntentDelegate.sol";
