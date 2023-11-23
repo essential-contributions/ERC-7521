81bee6a31caaf258e9e1d10f6e9a6e9794892758
PixelCircuits <circuitpixels@gmail.com>
2023-11-23 11:45:56 -0500

    formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,10 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-foundry";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-foundry';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.22",
+    version: '0.8.22',
     settings: {
       optimizer: {
         enabled: true,
@@ -13,10 +13,10 @@ const config: HardhatUserConfig = {
     },
   },
   paths: {
-    tests: "./test/ethers",
+    tests: './test/ethers',
   },
   typechain: {
-    outDir: "typechain",
+    outDir: 'typechain',
   },
 };
 
