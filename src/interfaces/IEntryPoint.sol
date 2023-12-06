2c755d1827ee842a15b49b8b353e38c42a9468f5
Bikem <bengisuozaydin@gmail.com>
2023-12-06 22:10:41 +0000

    undo split of intent validation and execution

--- src/interfaces/IEntryPoint.sol
+++ src/interfaces/IEntryPoint.sol
@@ -3,13 +3,30 @@ pragma solidity ^0.8.22;
 
 import {BaseIntentStandard} from "./BaseIntentStandard.sol";
 import {IIntentStandardRegistry} from "./IIntentStandardRegistry.sol";
-import {IIntentValidatorExecutor} from "./IIntentValidatorExecutor.sol";
 import {INonceManager} from "./INonceManager.sol";
 import {IntentSolution} from "./IntentSolution.sol";
 import {IAggregator} from "./IAggregator.sol";
 import {UserIntent} from "./UserIntent.sol";
 
-interface IEntryPoint is INonceManager {
+interface IEntryPoint is INonceManager, IIntentStandardRegistry {
+    /**
+     * An event emitted after each successful intent solution
+     * @param intentHash - unique identifier for the intent (hash its entire content, except signature).
+     * @param sender - the account that generates this intent.
+     * @param submitter - the account that submitted the solution for the intent.
+     */
+    event UserIntentEvent(bytes32 indexed intentHash, address indexed sender, address indexed submitter);
+
+    /**
+     * a custom revert error of handleIntents, to identify the offending intent.
+     * @param intIndex - index into the array of intents to the failed one
+     * @param segIndex - index into the array of intent segments to the failed one
+     * @param reason - revert reason
+     *  Should be caught in off-chain handleIntents simulation and not happen on-chain.
+     *  Useful for mitigating DoS attempts against solvers or for troubleshooting of solution/intent reverts.
+     */
+    error FailedIntent(uint256 intIndex, uint256 segIndex, string reason);
+
     /**
      * Execute a batch of UserIntents with given solution.
      * @param solution the UserIntents solution.
