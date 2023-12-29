b1c6c26c87f9b9798b919b700d2974c4b0f774e0
PixelCircuits <circuitpixels@gmail.com>
2023-12-29 14:20:23 -0500

    removed compression from coverage

--- src/utils/compression/DataRegistry.sol
+++ src/utils/compression/DataRegistry.sol
@@ -110,4 +110,11 @@ contract DataRegistry {
     function fnSel(uint256 index) external view returns (bytes4) {
         return fnSelRegistry[index];
     }
+
+    /**
+     * Add a test to exclude this contract from coverage report
+     * note: there is currently an open ticket to resolve this more gracefully
+     * https://github.com/foundry-rs/foundry/issues/2988
+     */
+    function test() public {}
 }
--- src/utils/compression/StatefulAbiEncoding.sol
+++ src/utils/compression/StatefulAbiEncoding.sol
@@ -256,6 +256,13 @@ abstract contract StatefulAbiEncoding {
             }
         }
     }
+
+    /**
+     * Add a test to exclude this contract from coverage report
+     * note: there is currently an open ticket to resolve this more gracefully
+     * https://github.com/foundry-rs/foundry/issues/2988
+     */
+    function test() public {}
 }
 
 /*
